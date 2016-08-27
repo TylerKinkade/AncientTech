0d664cc83d12cdaa5b4026bcac5cb4f362a3aa14
Tyler Kirk <TylerKinkade@gmail.com>
2016-08-27 16:55:46 -0700

    Additional structural changes
    Parameters of SDL_CreateWindow now set the window to the Center of the Monitor.
    Event Create to allow the Window to actually Close.
    Surface Variable Created to be applied with the Renderer in the Initialization of the Texture Variable. 
    Rectangle Variable Draws to the Screen with Applied Texture.

---
+++ AncientTech/Src/Core/Logger.h
@@ -0,0 +1,8 @@
+#ifndef LOGGER
+#define LOGGER
+
+/*static void Log(std::ostream &os, std::string &msg)
+{
+
+}*/
+#endif
--- AncientTech/Src/LaunchWin.cpp
+++ AncientTech/Src/LaunchWin.cpp
@@ -1,6 +1,18 @@
 #include <SDL.h>
+#include "Core\GameState.h"
+#include "Core\FileUtils.h"
 
+
+//todo: open window.
+//create sprite factory
+//asset manager
+//
 int main(int argc, char** argv)
 {
+	isRunning = true;
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* wnd = SDL_CreateWindow("A.T.", 100, 100, viewWidth, viewHeight, SDL_WINDOW_SHOWN);
+	SDL_Renderer* rend = SDL_CreateRenderer(wnd, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	while (isRunning) {}
 	return 0;
 }
