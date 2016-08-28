75fea4ca975cc8ea0a51167933f7c1cd2eea965c
Christopher Lozano <UchihaKite@gmail.com>
2016-08-28 13:35:08 -0700

    Centered the Window. Created a Surface Variable to House the Location of the BMP for the Texture. Surface to be Applied to the Initialization of the Texture Variable, with the Renderer. Rectangle Shape Variable created and Everything is Drawn in the Window.

--- AncientTech/Src/LaunchWin.cpp
+++ AncientTech/Src/LaunchWin.cpp
@@ -11,8 +11,27 @@ int main(int argc, char** argv)
 {
 	isRunning = true;
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* wnd = SDL_CreateWindow("A.T.", 100, 100, viewWidth, viewHeight, SDL_WINDOW_SHOWN);
+	SDL_Window* wnd = SDL_CreateWindow("A.T.", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, viewWidth, viewHeight, SDL_WINDOW_SHOWN);
 	SDL_Renderer* rend = SDL_CreateRenderer(wnd, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	while (isRunning) {}
+	SDL_Surface* wndSurface = SDL_LoadBMP("Textures/Circuit.bmp");
+	SDL_Texture* Txt = SDL_CreateTextureFromSurface(rend, wndSurface);
+
+	SDL_Rect drawingRect;
+	drawingRect.x = viewWidth/2;
+	drawingRect.y = viewHeight/2;
+	drawingRect.w = 100;
+	drawingRect.h = 200;
+
+	SDL_Event close;
+	while (isRunning) {
+		while (SDL_PollEvent(&close) != 0){
+			if (close.type == SDL_QUIT)
+				isRunning = false;
+		}
+		SDL_RenderClear(rend);
+		SDL_RenderCopy(rend, Txt, NULL, &drawingRect);
+		SDL_RenderPresent(rend);
+		SDL_UpdateWindowSurface(wnd);
+	}
 	return 0;
 }
